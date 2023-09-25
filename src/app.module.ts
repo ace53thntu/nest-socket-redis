a69012199e6cc321f6bccb410cd531d315522274
Em Dinh <ace53thntu@gmail.com>
2023-09-25 15:08:28 +0700

    setup chat gateway

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { ChatGateway } from './chat/chat.gateway';
 import configs from './configs';
 
 @Module({
@@ -12,6 +13,6 @@ import configs from './configs';
     }),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ChatGateway],
 })
 export class AppModule {}
